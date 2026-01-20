b73c693a11d92fd646230d5377f455228209952c
Garv Goel <goelgarv99@gmail.com>
2026-01-20 18:07:04 +0530

    Redux added to the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { Provider } from "react-redux";
+import appStore from "./Utils/appStore";
 import Body from "./Components/Body";
 function App() {
-  return <Body />;
+  return (
+    <Provider store={appStore}>
+      <Body />
+    </Provider>
+  );
 }
 
 export default App;
