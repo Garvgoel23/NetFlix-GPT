c9b7070125955387df8f34a4e9a6229fa18f1225
Garv Goel <goelgarv99@gmail.com>
2026-01-23 15:39:15 +0530

    made the gptSearch component

--- src/Components/GptSearch.tsx
+++ src/Components/GptSearch.tsx
@@ -1,4 +1,20 @@
+import { BgLogo } from "../Utils/constants";
+import GptMovieSuggestion from "./GptMovieSuggestion";
+import GptSearchBar from "./GptSearchBar";
+
 const GptSearch = () => {
-  return <div></div>;
+  return (
+    <div>
+      <div className="absolute -z-10">
+        <img
+          src={BgLogo}
+          alt="Background"
+          className="w-screen h-screen object-cover"
+        />
+      </div>
+      <GptSearchBar />
+      <GptMovieSuggestion />
+    </div>
+  );
 };
 export default GptSearch;
